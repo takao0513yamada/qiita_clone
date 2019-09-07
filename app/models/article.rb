e928b5ee1a09a33b054ce3b6f4b91a918ce1ef9d
taka president <life.i.s.w---@i.softbank.jp>
2019-09-08 06:00:12 +0900

    Execute 'bundle exec annotate'

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  title      :string(255)
+#  body       :text(65535)
+#  user_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ApplicationRecord
   belongs_to :user
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  title      :string(255)
+#  body       :text(65535)
+#  user_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
